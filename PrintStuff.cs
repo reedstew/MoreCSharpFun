acbc95e6214477b866689c6f30d4b95a65be7303
stewyman <stewyman@REEDSTEWARTE3F6>
2024-01-17 10:25:57 -0700

    Add project files.

---
+++ PrintStuff.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreCSharpFun
+{
+    internal class PrintStuff
+    {
+        private string language;
+        // Constructor
+
+        public PrintStuff()
+        {
+            language = "EN";
+        }
+        public PrintStuff(string temp) 
+        {
+            language = temp;
+        }
+        public void PrintName(string n)
+        {
+            if (language == "EN")
+            {
+                System.Console.WriteLine("Hello, " + n + "!");
+            }
+
+            if (language == "PT")
+            {
+                System.Console.WriteLine("Oi, " + n + "!");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using MoreCSharpFun;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        PrintStuff ps = new PrintStuff();
+
+        string name = "";
+
+        System.Console.WriteLine("Please enter your name");
+        name = System.Console.ReadLine();
+
+        ps.PrintName(name);
+    }
+}
